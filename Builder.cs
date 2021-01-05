2ce18d2e2fccf2a1b17d87bc005d82f0c804e924
KeshinSaito <50670419+KeshinSaito@users.noreply.github.com>
2021-01-05 05:01:06 +0500

    Add files via upload

---
+++ Builder.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Collections.Generic;
+
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Builder : MonoBehaviour
+{
+    public  Builder() 
+    {
+
+    }
+
+    [SerializeField] private Transform pointBuild;
+    [SerializeField] private GameObject roomForBuild;
+
+    public List<string> usedQuestions;
+
+    private GameObject rightRoom, leftRoom, forwardRoom;
+
+    private void Awake()
+    {
+        usedQuestions = new List<string>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MainBuildRoom();
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    private void MainBuildRoom()
+    {
+        switch (gameObject.tag)
+        {
+            case "RightBuilder":
+
+                rightRoom = Instantiate(roomForBuild, pointBuild);
+                rightRoom.tag = "RightRoom";
+                
+                break;
+
+            case "LeftBuilder":
+                leftRoom = Instantiate(roomForBuild, pointBuild);
+                leftRoom.tag = "LeftRoom";
+
+                break;
+
+            case "ForwardBuilder":
+                forwardRoom = Instantiate(roomForBuild, pointBuild);
+                forwardRoom.tag = "ForwardRoom";
+
+                break;
+        }
+      
+    }
+
+    public void BuildRightRoom(Transform _pointBuild)
+    {
+
+        rightRoom = Instantiate(roomForBuild, _pointBuild);
+        rightRoom.tag = "RightRoom";
+
+    }
+}
---
+++ ControllerRoom.cs
@@ -0,0 +1,108 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerRoom : MonoBehaviour
+{
+    [Tooltip("Lenght textQustions = lenght questions")]
+    [SerializeField] private Text[] questions;
+
+    [Tooltip("Lenght textQustions = lenght questions")]
+    [SerializeField] private string[] textsQuestions;
+
+    [SerializeField] private Transform pointBuild;
+
+    public Builder builder;
+    private bool isFirstQuestion;
+
+    private void Start()
+    {
+        GetBuilder();
+        SetQustions();
+        
+
+
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        builder.BuildRightRoom(pointBuild);
+    }
+
+    private void SetQustions()
+    {
+        if(builder.usedQuestions.Count == 0)
+        {
+            isFirstQuestion = true;
+            foreach (Text question in questions)
+            {
+                string placingQustion = textsQuestions[Random.Range(0, textsQuestions.Length - 1)];
+                if (!isFirstQuestion)
+                {
+                    foreach (string usedQuestion in builder.usedQuestions)
+                    {
+                        if (placingQustion != usedQuestion )
+                        {
+                            builder.usedQuestions.Add(placingQustion);
+                            question.text = placingQustion;
+                            isFirstQuestion = false;
+                            break;
+                        }
+                        else
+                        {
+                            Debug.LogWarning("Qustion >> " + placingQustion + " was placced");
+                            continue;
+                        }
+                    }
+                }
+                else
+                {
+                    builder.usedQuestions.Add(placingQustion);
+                    question.text = placingQustion;
+                    isFirstQuestion = false;
+                }
+            }
+        }
+        else if(builder.usedQuestions.Count != 0)
+        {
+            
+            foreach (Text question in questions)
+            {
+                string placingQustion = textsQuestions[Random.Range(0, textsQuestions.Length-1)];
+                foreach (string usedQuestion in builder.usedQuestions)
+                {
+                   
+                    if (usedQuestion != placingQustion)
+                    {
+                        builder.usedQuestions.Add(placingQustion);
+                        question.text = placingQustion;
+                        break;
+                    } 
+                    else if(usedQuestion == placingQustion)
+                    {
+                        Debug.LogWarning("Qustion >> " + placingQustion + " was placced");
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+    private void GetBuilder()
+    {
+        switch (gameObject.tag)
+        {
+            case "RightRoom":
+                builder = GameObject.FindGameObjectWithTag("RightBuilder").GetComponent<Builder>();
+                break;
+
+            case "LeftRoom":
+                builder = GameObject.FindGameObjectWithTag("LeftBuilder").GetComponent<Builder>();
+                break;
+
+            case "ForwardRoom":
+                builder = GameObject.FindGameObjectWithTag("ForwardBuilder").GetComponent<Builder>();
+                break;
+        }
+    }
+}
